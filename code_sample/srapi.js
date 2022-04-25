73ec1e1423ad64808d97af4f36db7b643045c731
eriemer1 <77909520+eriemer1@users.noreply.github.com>
2022-04-25 16:05:31 -0600

    Create srapi.js

---
+++ code_sample/srapi.js
@@ -0,0 +1,27 @@
+(function process( /*RESTAPIRequest*/ request, /*RESTAPIResponse*/ response) {
+ 
+    var queryParams = request.queryParams;
+    var number = queryParams.number; //get the value of the 'number' parameter from the request
+ 
+    var payload = {};
+    var grIncident = new GlideRecord('incident');
+    if (grIncident.get('number', number.toString())) {
+        payload = { //build the payload of details we will return
+           'number': grIncident.getValue('number'),
+           'state': grIncident.getDisplayValue('state'),
+           'short_description': grIncident.getValue('short_description'),
+           'assignment_group': grIncident.getDisplayValue('assignment_group'),
+           'description': grIncident.getValue('description'),
+           'assigned_to': grIncident.getDisplayValue('assigned_to'),
+           'cmdb_ci_name': grIncident.getDisplayValue('cmdb_ci'),
+           'caller_id_name': grIncident.getDisplayValue('caller_id'),
+           'category': grIncident.getDisplayValue('category')
+        };
+        response.setBody(payload); //set the Body to return
+        response.setStatus(200); //sett he HTTP Status to return
+    } else {//error handling, if we don't find an incident with a matching number send a 404 error with a message
+        response.setBody("Invalid Record Number");
+        response.setStatus(404);
+    }
+ 
+})(request, response);
